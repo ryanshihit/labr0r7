f4d6d708f67d352acd250badf44a01be5ab1b5d8
ryanshihit <ryan@ryanMacBook-Air.local>
2015-11-09 22:38:33 +0800

    R4

--- Test/views.py
+++ Test/views.py
@@ -3,5 +3,3 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import django
 
-def home(request):
-	return HttpResponse('<p>Django Version = ' + str(django.get_version()))
